89adcf8801cf172cfd58bd514fd4b9a3fcc16674
Zadu <jartetab@unicartagena.edu.co>
2025-09-26 16:59:21 -0500

    Ejercicio 3

---
+++ MotoSinOverride.java
@@ -0,0 +1,8 @@
+package Unidad_2_Taller_10_SobreEscrituraMetodos;
+
+public class MotoSinOverride extends Vehiculo{
+    //@Override
+    public void moverse(){
+        System.out.println("Hola");
+    }
+}
