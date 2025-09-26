78ed2b1206d4a02aca7e0d8226cc6a489f789c15
Zadu <jartetab@unicartagena.edu.co>
2025-09-26 17:02:53 -0500

    Ejercicio 2 corregido

--- Bicicleta.java
+++ Bicicleta.java
@@ -2,6 +2,7 @@ package Unidad_2_Taller_10_SobreEscrituraMetodos;
 
 public class Bicicleta extends Vehiculo{
     //@Override //Explicar por que da error al quitar el override y al cambiar la firma del metodo
+    //Se borra el override
     public void moverse(){
         System.out.println("Estoy en movimiento y soy una bicicleta");
     }
